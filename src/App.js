df3e128cb6f0f14cdbb415d8c9c36c81650af5ce
rbdevelop <robert@rbdevelop.com>
2018-09-23 21:49:48 -0500

    s3 bucket hosting info

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import FineUploaderS3 from 'fine-uploader-wrappers/s3';
+import FineUploaderTraditional from 'fine-uploader-wrappers';
 import Gallery from 'react-fine-uploader';
 
 import logo from './logo.png';
 import './App.css';
 import 'react-fine-uploader/gallery/gallery.css';
 
-const uploader = new FineUploaderS3({
+const uploader = new FineUploaderTraditional({
   options: {
     debug: true,
     request: {
-      endpoint: "https://fullfillment-engine-file-uploader.s3-us-east-2.amazonaws.com",
-      accessKey: process.env.S3_BUCKET,
-    },
-    signature: {
-      endpoint: "https://fullfillment-engine-file-uploader.s3-us-east-2.amazonaws.com",
-      version: 1
+      most: "POST",
+      endpoint: "https://1brxufrp5l.execute-api.us-east-1.amazonaws.com/qa/upload-file",
     },
     chunking: {
       enabled: true
     },
-    objectProperties: {
-      region: "us-east-2"
-    }
   }
 })
 
