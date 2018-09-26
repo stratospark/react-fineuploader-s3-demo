201720f4319aa494df664cc5e5faa423b23cc765
rbdevelop <robert@rbdevelop.com>
2018-09-26 16:23:40 -0500

    not using fine uploader and works now

--- src/App.js
+++ src/App.js
@@ -1,47 +1,61 @@
-import React, { Component } from 'react';
-import FineUploaderTraditional from 'fine-uploader-wrappers';
-import Gallery from 'react-fine-uploader';
-
-import logo from './logo.png';
+import React from 'react'
+import axios, { post } from 'axios';
 import './App.css';
-import 'react-fine-uploader/gallery/gallery.css';
 
-const uploader = new FineUploaderTraditional({
-  options: {
-    debug: true,
-    request: {
-      method: "POST",
-      endpoint: "https://1brxufrp5l.execute-api.us-east-1.amazonaws.com/qa/upload-file",
-      inputName: "file",
-    },
-    inputName: "file",
-    chunking: {
-      enabled: true
-    },
+class SimpleReactFileUpload extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state ={
+      file:null
+    }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.fileUpload = this.fileUpload.bind(this)
   }
-})
+  onFormSubmit(e){
+    e.preventDefault() // Stop form submit
+    this.fileUpload(this.state.file).then((response)=>{
+      console.log(response.data);
+    })
+  }
+  onChange(e) {
+    this.setState({file:e.target.files[0]})
+  }
+  fileUpload(file){
+    const url = 'https://1brxufrp5l.execute-api.us-east-1.amazonaws.com/qa/upload-file';
+    const formData = new FormData();
+    formData.append('file',file)
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data'
+        }
+    }
+    return  post(url, formData,config)
+  }
 
-class App extends Component {
   render() {
     return (
-      <div className="App">
+       <div className="App">
         <div className="header">
           <h1 className="title">Secure Data File Uploads for (ATP) service </h1>
           <p className="subTitle">
           Intended for BuckHead and FreshPoint data uploads
           </p>
         </div>
-        <Gallery 
-          className="gallery" 
-          uploader={uploader}
-        />
+      <form onSubmit={this.onFormSubmit}>
+        <h1>File Upload</h1>
+        <input type="file" onChange={this.onChange} />
+        <button type="submit">Upload</button>
         <div className="footer"> 
-        <p>Crafted by Sysco BT-Ignite </p>
+          <p>Crafted by Sysco BT-Ignite </p>
         </div>
+      </form>
       </div>
-
-    );
+   )
   }
 }
 
-export default App;
+
+
+export default SimpleReactFileUpload
