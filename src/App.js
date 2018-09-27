ac6df8a022ec02b95289474e9c85a9fb0017c4b9
rbdevelop <robert@rbdevelop.com>
2018-09-27 14:03:06 -0500

    successful save

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import axios, { post } from 'axios';
+import { post } from 'axios';
 import './App.css';
+import Alert from 'react-s-alert';
+import 'react-s-alert/dist/s-alert-default.css';
+import 'react-s-alert/dist/s-alert-css-effects/slide.css';
 
-class SimpleReactFileUpload extends React.Component {
+class App extends React.Component {
 
   constructor(props) {
     super(props);
@@ -12,13 +15,37 @@ class SimpleReactFileUpload extends React.Component {
     this.onFormSubmit = this.onFormSubmit.bind(this)
     this.onChange = this.onChange.bind(this)
     this.fileUpload = this.fileUpload.bind(this)
+    this.displaySuccess = this.displaySuccess.bind(this)
+    this.displayError = this.displayError.bind(this)
   }
   onFormSubmit(e){
     e.preventDefault() // Stop form submit
-    this.fileUpload(this.state.file).then((response)=>{
-      console.log(response.data);
+    this.fileUpload(this.state.file)
+    .then((response)=>{
+      console.log(response.status);
+        if (response.data !== 'OK') {
+          this.displayError();
+        }
+        else { 
+          this.displaySuccess();
+        }
     })
   }
+
+  displayError() {
+    Alert.error('Error Saving Data. Only CSV files accepted', {
+      position: 'top-right',
+      effect: 'slide',
+    });
+  }
+
+  displaySuccess() {
+    Alert.success('Save Successful', {
+      position: 'top-right',
+      effect: 'slide',
+    });
+  }
+
   onChange(e) {
     this.setState({file:e.target.files[0]})
   }
@@ -43,6 +70,7 @@ class SimpleReactFileUpload extends React.Component {
           Intended for BuckHead and FreshPoint data uploads
           </p>
         </div>
+      <Alert timeout={5000}/>
       <form onSubmit={this.onFormSubmit}>
         <h1>File Upload</h1>
         <input type="file" onChange={this.onChange} />
@@ -58,4 +86,4 @@ class SimpleReactFileUpload extends React.Component {
 
 
 
-export default SimpleReactFileUpload
+export default App
